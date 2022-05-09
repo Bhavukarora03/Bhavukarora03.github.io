1adf8c8db22b35e5790fa571707043d880028dca
Bhavuk <Bhavuk.arora03@gmail.com>
2022-05-10 04:08:38 +0530

    changes pass

--- handler.php
+++ handler.php
@@ -22,9 +22,9 @@ try {
 	$mail->isSMTP(); // Send using SMTP
 	$mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
 	$mail->SMTPAuth = true; // Enable SMTP authentication
-	$mail->Username = 'Bhavuk.arora03@gmail.com'; // SMTP username
-	$mail->Password = 'Damocles2X@'; // SMTP password
-	$mail->SMTPSecure = 'ssl'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	$mail->Username = 'bhavuk.arora03@gmail.com'; // SMTP username
+	$mail->Password = 'xkurzkwmxguuhngi'; // SMTP password
+	$mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 	$mail->setFrom($mail_from_email, $mail_from_name); // Your email
